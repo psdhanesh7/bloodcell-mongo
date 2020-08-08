72bc33ad9b706321ab9bf386b796a3f7d70f5449
psdhanesh7 <49329403+psdhanesh7@users.noreply.github.com>
2020-08-08 19:49:21 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://psdhanesh7:ke6DiynVJAyK4cb@cluster0.llxb8.mongodb.net/bloodcell-dev?retryWrites=true&w=majority",
-    jwtSecret: 'thisIsAVEryAwesomeeeSecretKeyWOrd'
-}
